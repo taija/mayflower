07aea101c1f3ce1e0792cdedc19d1bb1565b05ea
Erik Steen <erik.steen@bellevuecollege.edu>
2013-07-10 10:30:02 -0700

    Fix 404 page links and add searchform

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form id="search" method="get" action="<?php echo 'http://bellevuecollege.edu/search/' ?>">
+	<fieldset>
+		<input id="widget-s" class="regular-text" name="txtQuery" value="" size="30" type="text" /> 
+		<input id="searchsubmit" value="Search" type="submit" />
+	</fieldset>
+</form>
