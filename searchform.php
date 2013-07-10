b3042f33de3b33c6e54a8b63234e50b7beb2809d
Juan Ulloa <juan.ulloa@bellevuecollege.edu>
2013-07-10 13:08:28 -0700

    clean up code - nothing noticeble

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form class="form-search" method="get" action="<?php echo 'http://bellevuecollege.edu/search/' ?>">
+<form class="form-search" method="get" action="http://bellevuecollege.edu/search/">
 	<fieldset>
 		<input type="text" name="txtQuery"> 
 		<button type="submit" class="btn">Search</button>
