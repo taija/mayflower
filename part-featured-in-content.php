5084e4983cb412e51cc6a52302ddf1ec15594e48
Erik Steen <erik.steen@bellevuecollege.edu>
2013-07-15 13:38:34 -0700

    Update content-padding class

--- part-featured-in-content.php
+++ part-featured-in-content.php
@@ -1,126 +1,155 @@
-	<div id="myCarousel" class="carousel slide">
-		<div class="carousel-inner">
-			<?php
-			$the_query = new WP_Query(array(
-				'post_type'=>'slider',
-				'orderby' => 'menu_order',
-				'order'=> 'ASC',
-				'posts_per_page' => 1,
-			));
-			while ( $the_query->have_posts() ) :
-			$the_query->the_post();
-			?>
-
-			<div class="item active">
-				<?php
-			        // If a post class has input, sanitize it and add it to the post class array.
-					$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
-				        if ( !empty( $slider_ext_url ) )
-					{ ?>
-
-					<h2>
-						<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php echo $slider_ext_url; ?>"><?php the_post_thumbnail('featured-in-content');?></a>
-					</h2>
-
-					<?php } else { ?>
-
-				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-in-content');?></a>
-				<?	} //end else ?>
-			<?php
-				$options = get_option( 'mayflower_slider_options' );
-					if ( ! isset( $options['theme_slider_title_show'] ) )
-						$options['theme_slider_title_show'] = 0;
-
-					if ( $options['theme_slider_title_show'] == 1 ) { ?>
-
-				<div class="carousel-caption">
+<?php if ( is_front_page() ) {
+    $mayflower_options = mayflower_get_options();
+    if( $mayflower_options['slider_toggle'] === true && $mayflower_options['slider_layout'] === 'featured-in-content') { ?>
+	
+				<div id="myCarousel" class="carousel slide">
+					<div class="carousel-inner">
 						<?php
-					        // If a post class has input, sanitize it and add it to the post class array.
-							$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
-						        if ( !empty( $slider_ext_url ) )
-							{ ?>
-
-							<h2>
-								<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php echo $slider_ext_url; ?>"><?php the_title(); ?></a>
-							</h2>
-
-							<?php } else { ?>
-								<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-
-						<?	} //end else ?>
-					<?php the_excerpt(); ?>
-				</div>
-			<?php } else  { } ?>
-
-			</div><!-- item active -->
-
-			<?php
-				endwhile;
-					wp_reset_postdata();
-			?>
-
-			<?php
-				$the_query = new WP_Query(array(
-					'post_type'=>'slider',
-					'orderby' => 'menu_order',
-					'order'=> 'ASC',
-					'posts_per_page' => 4,
-					'offset' => 1
-				));
-				while ( $the_query->have_posts() ) :
-				$the_query->the_post();
-			?>
-			<div class="item">
-				<?php
-			        // If a post class has input, sanitize it and add it to the post class array.
-					$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
-				        if ( !empty( $slider_ext_url ) )
-					{ ?>
-
-					<h2>
-						<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php echo $slider_ext_url; ?>"><?php the_post_thumbnail('featured-in-content');?></a>
-					</h2>
-
-					<?php } else { ?>
-
-				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-full');?></a>
-				<?	} //end else ?>
-
-			<?php
-				$options = get_option( 'mayflower_slider_options' );
-					if ( ! isset( $options['theme_slider_title_show'] ) )
-						$options['theme_slider_title_show'] = 0;
-
-					if ( $options['theme_slider_title_show'] == 1 ) { ?>
-
-					<div class="carousel-caption">
+						$the_query = new WP_Query(array(
+							'post_type'=>'slider',
+							'orderby' => 'menu_order',
+							'order'=> 'ASC',
+							'posts_per_page' => 1,
+						));
+						while ( $the_query->have_posts() ) :
+						$the_query->the_post();
+						?>
+			
+							<div class="item active">
+								<?php
+							        // If a post class has input, sanitize it and add it to the post class array.
+									$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
+								        if ( !empty( $slider_ext_url ) )
+									{ ?>
+				
+									<h2>
+										<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php the_title();; ?>"><?php the_post_thumbnail('featured-in-content');?></a>
+									</h2>
+				
+									<?php } else { ?>
+				
+								<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-in-content');?></a>
+								<?	} //end else ?>
+							<?php
+									//should we show title & excerpt?
+									$mayflower_options = mayflower_get_options();
+										if ($mayflower_options['slider_title'] == 'true' || $mayflower_options['slider_excerpt'] == 'true' ) { ?>
+		
+		
+										<div class="carousel-caption">
+											<?php
+												if ($mayflower_options['slider_title'] == 'true') {
+											        // If a post class has input, sanitize it and add it to the post class array.
+													$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
+												        if ( !empty( $slider_ext_url ) )
+													{ ?>
+		
+													<h2>
+														<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+													</h2>
+		
+													<?php } else { ?>
+														<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+		
+												<?	} //end else ?>
+											<?php } else { } ?>
+		
+											<?php if ($mayflower_options['slider_excerpt'] == 'true' ) { ?>
+												<?php the_excerpt(); ?>
+											<?php } else { } ?>
+		
+										</div><!-- carousel-caption -->
+		
+								<?php } else  { } ?>			
+							</div><!-- item active -->
+			
 						<?php
-					        // If a post class has input, sanitize it and add it to the post class array.
-							$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
-						        if ( !empty( $slider_ext_url ) )
-							{ ?>
+							endwhile;
+								wp_reset_postdata();
+						?>
+			
+						<?php
+							$the_query = new WP_Query(array(
+								'post_type'=>'slider',
+								'orderby' => 'menu_order',
+								'order'=> 'ASC',
+								'posts_per_page' => 4,
+								'offset' => 1
+							));
+							while ( $the_query->have_posts() ) :
+							$the_query->the_post();
+						?>
+						
+						<div class="item">
+							<?php
+						        // If a post class has input, sanitize it and add it to the post class array.
+								$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
+							        if ( !empty( $slider_ext_url ) )
+								{ ?>
+			
+								<h2>
+									<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('featured-in-content');?></a>
+								</h2>
+			
+								<?php } else { ?>
+			
+							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('featured-full');?></a>
+							<?	} //end else ?>
+			
+						<?php
+								//should we show title & excerpt?
+								$mayflower_options = mayflower_get_options();
+									if ($mayflower_options['slider_title'] == 'true' || $mayflower_options['slider_excerpt'] == 'true' ) { ?>
+	
+	
+									<div class="carousel-caption">
+										<?php
+											if ($mayflower_options['slider_title'] == 'true') {
+										        // If a post class has input, sanitize it and add it to the post class array.
+												$slider_ext_url = get_post_meta($post->ID, 'slider_url', true);
+											        if ( !empty( $slider_ext_url ) )
+												{ ?>
+	
+												<h2>
+													<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+												</h2>
+	
+												<?php } else { ?>
+													<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+	
+											<?	} //end else ?>
+										<?php } else { } ?>
+	
+										<?php if ($mayflower_options['slider_excerpt'] == 'true' ) { ?>
+											<?php the_excerpt(); ?>
+										<?php } else { } ?>
+	
+									</div><!-- carousel-caption -->
+	
+							<?php } else  { } ?>
+			
+						</div><!-- item -->
+			
+						<?php
+							endwhile;
+							wp_reset_postdata();
+						?>
+					</div><!-- carousel-inner -->
+			
+					<?php
+						$published_posts = wp_count_posts('slider')->publish;
+						if ($published_posts >1 ) { ?>
+							<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+							<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+					<?php } else //don't show controls ?>
+			
+				</div><!-- #myCarousel -->
 
-							<h2>
-								<a href="<?php echo esc_url($slider_ext_url);?>" title="<?php echo $slider_ext_url; ?>"><?php the_title(); ?></a>
-							</h2>
 
-							<?php } else { ?>
-								<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
 
-						<?	} //end else ?>
-						<?php the_excerpt(); ?>
-					</div>
-			<?php } else  { } ?>
-
-			</div><!-- item -->
-
-			<?php
-				endwhile;
-				wp_reset_postdata();
-			?>
-		</div><!-- carousel-inner -->
-
-		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-
-	</div><!-- #myCarousel -->
+		<?php } elseif( $mayflower_options['slider_toggle'] == 'false' ) { } ?>
+<?php
+} else {
+	// This is not a homepage so display nothing
+		}
+?>
