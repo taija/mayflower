ccbd0b3226f04b16391cb5526c74cffb9f9e3754
Erik Steen <erik.steen@bellevuecollege.edu>
2013-07-22 09:59:53 -0700

    Update index.php with Class schedule shortcodes

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
+
 	<?php
 		$mayflower_options = mayflower_get_options();
 		$current_layout = $mayflower_options['default_layout'];
+
+		 //echo do_shortcode('[AllClassInformation course="ECON"]');
+		 //echo do_shortcode('[OneClassInformation course="ABE" number="042"]');
 	?>
 		<?php
 				// Sidebar Layout
