daef1030c4480db3761ab88125cfb14f1aa95eed
Juan Ulloa <juan.ulloa@bellevuecollege.edu>
2013-08-06 12:10:10 -0700

    remove texture background image and replace with js

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
         
 <script src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/site.js"></script>
 <?php wp_footer(); ?>
 
 <?php
