d06c2b10eed39071f21584b214153efb3050920d
Juan Ulloa <juan.ulloa@bellevuecollege.edu>
2013-10-04 14:24:58 -0700

    differentiate between sidebar menu on left and right

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,11 @@
-<div class="sidebar span3">
+<div class="sidebar span3 <?php
+	$mayflower_options = mayflower_get_options();
+	$current_layout = $mayflower_options['default_layout'];
+	if ( $current_layout == 'sidebar-content' ) { 
+		?>sidebarleft<?php
+	} else {
+		?>sidebarright<?php
+	}; ?>">
 		<?php dynamic_sidebar( 'global-widget-area' ); ?>
 
 		<?php if( is_home() || is_single() ) :?>
