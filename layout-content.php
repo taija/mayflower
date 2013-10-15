e8c1ad011efa8e4bbb77b30abd0cd09f6be6f43f
Juan Ulloa <juan.ulloa@bellevuecollege.edu>
2013-10-15 07:35:18 -0700

    fix bug where page title doesnt show if there is no page content

--- layout-content.php
+++ layout-content.php
@@ -26,32 +26,46 @@ global $mayflower_brand;
 	} else { 
 	
 		if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-
-			if($post->post_content=="") : ?>
-			<!-- Don't display empty the_content or surround divs -->
-	
-			<?php else : ?>
-			<!-- Do stuff when the_content has content -->
-				<div class="content-padding">
-					<?php if (is_front_page() ) {
-						//don't show the title on the home page
-						} else { ?>
-                        <?php 
-							if ( is_main_site()) {
-								if(intval($post->post_parent)>0){
-									?><h1><?php the_title(); ?></h1><?php
-								}
-							} else {
-								?><h1><?php the_title(); ?></h1><?php
-							}	
-						}; ?>
-					<?php the_content(); ?>
-				</div><!-- .content-padding -->
-	
-			<?php endif; 
-			
-			get_template_part('part-blogroll');
-			
+?>
+        
+            <div class="content-padding <?php 
+                //display slider in content if option is selected
+                if ( ($mayflower_options['slider_toggle'] == 'true') && ($mayflower_options['slider_layout'] == 'featured-in-content')){ 
+                    echo "row-padding";
+                } 
+            ?>">
+            <?php
+                
+            if (is_front_page() ) {
+                //don't show the title on the home page
+            } else { 
+                if ( is_main_site()) {
+                    //if main site, only show title here if page is not top-most ancestor
+                    if(intval($post->post_parent)>0){
+                        ?><h1><?php the_title(); ?></h1><?php
+                    }
+                } else {
+                    ?><h1><?php the_title(); ?></h1><?php
+                }
+            }; ?>
+            
+            </div><!--.content-padding-->
+            
+            <?php
+            if($post->post_content=="") : ?>
+                <!-- Don't display empty the_content or surround divs -->
+                <?php
+            else : ?>	
+                <div class="content-padding">
+                <?php 
+                the_content(); ?>
+                </div> <!--.content-padding-->
+                <?php 
+            endif; 
+                        
+            get_template_part('part-blogroll'); ?>
+            
+		<?php
 		endwhile; else: ?>
 		<p><?php _e('Sorry, these aren\'t the bytes you are looking for.'); ?></p>
 		<?php endif; 
