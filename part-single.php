fa473fd85c4e92b20f8bd9c35b5b5155f31ab9a1
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2013-11-20 14:17:23 -0800

    Load medium sized images for single post template
    Resolved task 4513. Changed the function to load medium sized images
    instead of thumbnails.

--- part-single.php
+++ part-single.php
@@ -16,11 +16,11 @@
 
 					 <div class="pull-left wp-caption">
 						<?php
-							the_post_thumbnail('thumbnail', array('class' => 'media-object'));
+							the_post_thumbnail('medium', array('class' => 'media-object'));
 								if(get_post(get_post_thumbnail_id())->post_excerpt) {
                                     $tn_id = get_post_thumbnail_id( $post->ID );
 
-                                    $img = wp_get_attachment_image_src( $tn_id, 'thumbnail' );
+                                    $img = wp_get_attachment_image_src( $tn_id, 'medium' );
                                     $width = $img[1];
                                     ?>
 								<p class="featured-caption media-object wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
