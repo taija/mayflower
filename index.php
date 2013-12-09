86246b61241d32fcae7321a2575e4e6b81d3e433
Erik Steen <erik.steen@bellevuecollege.edu>
2013-12-09 14:17:39 -0800

    Rework Template Hierarchy for collapsing content when no sidebar
    This is a big change. For further details see FogBugz item #4515

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php get_header(); 
+index.php<?php get_header(); 
 
-$mayflower_options = mayflower_get_options();
-$current_layout = $mayflower_options['default_layout'];
 
  //echo do_shortcode('[AllClassInformation course="ECON"]');
  //echo do_shortcode('[OneClassInformation course="ABE" number="042"]');
@@ -11,12 +9,9 @@ $current_layout = $mayflower_options['default_layout'];
 if ( is_main_site() && is_front_page() ) {
 	//Is BC Home page
 	get_template_part('template-front-page');
-} else if ( $current_layout == 'content-sidebar' || $current_layout == 'sidebar-content' ) {
-	//Sidebar layout
-	get_template_part('layout-sidebar');
 } else { 
-	//No sidebar layout
-	get_template_part('layout-content');
+	// Layout
+	get_template_part('layout');
 }
 		 
 get_footer(); ?>
