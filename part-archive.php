0bf338ed36c457c25ad8bc2551c9e0aa1ba3a11e
Erik Steen <erik.steen@bellevuecollege.edu>
2013-12-10 08:59:28 -0800

    Page template for category archives
    Implements the Style Category View task in Fogbugz #4403

---
+++ part-archive.php
@@ -0,0 +1,89 @@
+	<div class="content-padding">
+
+					<?php if (is_category()) { ?>
+						<h2 class="archive_title h2">
+							<?php single_cat_title(); ?>
+						</h2>
+					<?php } elseif (is_tag()) { ?> 
+						<h2 class="archive_title h2">
+							<?php single_tag_title(); ?>
+						</h2>
+					<?php } elseif (is_author()) { ?>
+						<h2 class="archive_title h2">
+							<?php get_the_author_meta('display_name'); ?>
+						</h2>
+					<?php } elseif (is_day()) { ?>
+						<h2 class="archive_title h2">
+							<?php the_time('l, F j, Y'); ?>
+						</h2>
+					<?php } elseif (is_month()) { ?>
+					    <h2 class="archive_title h2">
+					    	<?php the_time('F Y'); ?>
+					    </h2>
+					<?php } elseif (is_year()) { ?>
+					    <h2 class="archive_title h2">
+					    	<?php the_time('Y'); ?>
+					    </h2>
+					<?php } ?>
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+						<h2 <?php post_class() ?>>
+							<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+						</h2>
+
+						<small>Date posted: <?php echo get_the_date(); ?></small>
+
+
+						<div class="media">
+						    <a class="pull-left" href="<?php the_permalink(); ?>">
+							<?php
+								if ( has_post_thumbnail() ) {
+									the_post_thumbnail('thumbnail', array('class' => 'media-object'));
+										if(get_post(get_post_thumbnail_id())->post_excerpt) { ?>
+											<span class="featured-caption media-object"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></span>
+										<?php } ?>
+							<?php } else { } ?>
+						    </a>
+
+							<div class="media-body">
+
+								<div class="media-content">
+									<?php the_excerpt(); ?>
+								</div><!-- media-content -->
+							    <?php
+								if (is_single($post)){
+								?>
+
+						        <?php
+								} else {
+								?>
+						           <p>
+									<a class="btn btn-small primary-read-more" href="<?php the_permalink(); ?>">
+									Read More <i class="icon-chevron-right"></i>
+							            </a>
+						            </p>
+						        <?php
+
+								}
+								?>
+						    </div><!-- media-body -->
+						</div><!-- media -->
+						<hr />
+				<?php endwhile;?>
+			</div><!-- content-padding -->
+				<?php wp_reset_postdata(); ?>
+	
+					
+					<ul class="pager content-padding">
+						<li>
+							<?php previous_posts_link('<i class="icon-chevron-left"></i> Previous Page'); ?>
+						</li>
+						<li>
+							<?php next_posts_link('Next page <i class="icon-chevron-right"></i>'); ?>
+						</li>
+					</ul>
+					<?php wp_reset_query(); ?>								
+					
+					<?php else : ?>					
+					<?php endif; ?>
