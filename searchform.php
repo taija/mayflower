838d1308fbf66c9dcc05dfebf633d479c91b4136
Erik Steen <erik.steen@bellevuecollege.edu>
2014-06-23 09:06:17 -0700

    Update search form to use bootstrap 3

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
 <form class="form-search" method="get" action="http://bellevuecollege.edu/search/">
 	<fieldset>
-		<input type="text" name="txtQuery"> 
-		<button type="submit" class="btn">Search</button>
+    <div class="input-group">
+      <input type="text" name="txtQuery" class="form-control">
+      <span class="input-group-btn btn-default">
+        <button class="btn btn-default" type="submit">Search</button>
+      </span><!-- input-group-btn -->
+    </div><!-- /input-group -->
 	</fieldset>
 </form>
