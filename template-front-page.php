7e6256a1b8a21c93f4d75f5519edb9720b2e825f
Erik Steen <erik.steen@bellevuecollege.edu>
2014-07-28 09:51:53 -0700

    Update Apply button on home page per Enrollment's request

--- template-front-page.php
+++ template-front-page.php
@@ -92,7 +92,7 @@ define('NEW_CATEGORY_NAME', "BC Homepage");
     	</div><!--.content-padding-->
 	</section><!--#home-events-->
 	<div id="home-sidelinks">
-		<p id="apply" ><a href="/enrollment/admissions/#content" class="btn btn-success"><strong>Apply</strong> for admissions</a></p>
+		<p id="apply" ><a href="/enrollment/admissions/#content" class="btn btn-success"><strong>Apply for admissions</strong>Fall application deadline Sept. 19</a></p>
 		<?php
 			$the_query = new WP_Query(array(
 				'post_type'=>'small_ad',
