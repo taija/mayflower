4f04a6fa581a2b1c42e897c61ae907af3ad39357
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-04-08 11:19:35 -0700

    Fix Mobile Display of Header

--- searchform.php
+++ searchform.php
@@ -1,11 +1,9 @@
-<form class="form-search" method="get" action="//www.bellevuecollege.edu/search/">
-	<fieldset>
-    <div class="input-group">
-	    <label class="sr-only" for="college-search-field-x">Search</label>
-      <input type="text" name="txtQuery" class="form-control" id="college-search-field-x">
-      <span class="input-group-btn btn-default">
-        <button class="btn btn-default" type="submit">Search</button>
-      </span><!-- input-group-btn -->
-    </div><!-- /input-group -->
-	</fieldset>
+<form action="//www.bellevuecollege.edu/search/" method="get" class="form-search">
+	<div class="input-group pull-right" role="search">
+	  <input type="text" value="" name="txtQuery" class="form-control" id="college-search-field">
+	  <label class="sr-only" for="college-search-field">Search</label>
+	  <span class="input-group-btn">
+			<button type="submit" class="btn btn-default" name="search" id="college-search-submit">Search</button>
+	  </span>
+	</div><!-- input-group -->
 </form>
