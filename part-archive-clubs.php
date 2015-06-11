437c6487c2cb2308c327c71cdb9b1d2721ddf6ca
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-06-11 10:55:18 -0700

    Sort part-archive-clubs Alphabetically and Style

--- part-archive-clubs.php
+++ part-archive-clubs.php
@@ -1,17 +1,33 @@
-	<div class="content-padding">				
+<?php
+if( is_tax() ) {
+    global $wp_query;
+    $term = $wp_query->get_queried_object();
+    $title = $term->name;
+    $description = $term->description;
+}
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<?php 
-						
-					?>
-					
-						<h2 <?php post_class() ?>>
+/* Sort Alphabetically */
+global $query_string;
+query_posts( $query_string . '&orderby=title&order=ASC' );
+?>
+<div class="content-padding">
+    <h1><?php if( is_tax() ) {
+                echo $title;
+              }
+        ?> Student Clubs</h1>
+        <?php if( is_tax() && !empty($description)) {
+                ?><p class="lead"><?php echo $description; ?></p>
+    <?php }
+            ?><ul><?php
+                    
+					 if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        
+						<li <?php post_class() ?>>
 							<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-						</h2>
+						</li>
 
-						<small>Date posted: <?php echo get_the_date(); ?></small>	
-										
 				<?php endwhile;?>
+            </ul>
 			</div><!-- content-padding -->
 				<?php wp_reset_postdata(); ?>
 	
@@ -24,7 +40,7 @@
 							<?php next_posts_link('Next page <i class="icon-chevron-right"></i>'); ?>
 						</li>
 					</ul>
-					<?php wp_reset_query(); ?>								
+					<?php wp_reset_query(); ?>
 					
-					<?php else : ?>					
+					<?php else : ?>
 					<?php endif; ?>
