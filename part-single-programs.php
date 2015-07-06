0c7395b4ff273829b6cf745a0b8c02bd51ad2a60
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-07-06 10:43:23 -0700

    Fix errors in part-single-programs
    * Fix spelling of program_url
    * Remove check for ‘is_chartered’ on budget link

--- part-single-programs.php
+++ part-single-programs.php
@@ -4,7 +4,7 @@
     https://github.com/BellevueCollege/student-programs
 */
 
-if (have_posts()) : 
+if (have_posts()) :
 	while (have_posts()) : the_post();
 		$program_name = get_post_meta( get_the_ID(), 'program_name', true );
 		$program_description = get_post_meta( get_the_ID(), 'program_description', true );
@@ -13,7 +13,7 @@ if (have_posts()) :
 		$program_contact_phone = get_post_meta( get_the_ID(), 'program_contact_phone', true );
 		$Office_location = get_post_meta( get_the_ID(), 'Office_location', true );
 		$office_hours = get_post_meta( get_the_ID(), 'office_hours', true );
-		$prgoram_url = get_post_meta( get_the_ID(), 'prgoram_url', true );
+		$program_url = get_post_meta( get_the_ID(), 'program_url', true );
 		$budget_document_link = get_post_meta( get_the_ID(), 'budget_document_link', true );
 		
 
@@ -48,17 +48,17 @@ if (have_posts()) :
 					<li>Office Hours: <strong><?php echo $office_hours; ?></strong></li>
 				<?php endif; ?>
 
-				<?php if( !empty( $prgoram_url ) ) : ?>
-					<li>Program Website: <strong><a href="<?php echo $prgoram_url; ?>"> <?php echo $prgoram_url; ?> </a></strong></li>
+				<?php if( !empty( $program_url ) ) : ?>
+					<li>Program Website: <strong><a href="<?php echo $program_url; ?>"> <?php echo $program_url; ?> </a></strong></li>
 				<?php endif; ?>
 				
 			</ul>
 			
 			<?php the_content(); ?>
-			<? if( !empty( $budget_document_link ) && $is_chartered ) : ?>
+			<? if( !empty( $budget_document_link ) ) : ?>
 				<p><a href="<?php echo $budget_document_link; ?>" target="_blank">View Current Budget Information (opens in new window)</a></p>
 			<?php endif; ?>
-		</div><!--.content-padding-->     
+		</div><!--.content-padding-->
 
 	<?php endwhile;
 	wp_reset_query();
