62c51c14ae35a9794c227042798d687319145c4d
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-08-03 13:55:20 -0700

    Remove unused function mayflower_get_paginate_archive_page_links

--- inc/functions/custom.php
+++ inc/functions/custom.php
@@ -177,41 +177,6 @@ function mayflower_get_page_tab_markup() {
 
 }
 
-/**
- * Paginate Archive Index Page Links
- */
-function mayflower_get_paginate_archive_page_links( $type = 'plain', $endsize = 1, $midsize = 1 ) {
-	global $wp_query, $wp_rewrite;
-	$wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
-
-	// Sanitize input argument values
-	if ( ! in_array( $type, array( 'plain', 'list', 'array' ) ) ) $type = 'plain';
-	$endsize = (int) $endsize;
-	$midsize = (int) $midsize;
-
-	// Setup argument array for paginate_links()
-	$pagination = array(
-		'base' => @add_query_arg('paged','%#%'),
-		'format' => '',
-		'total' => $wp_query->max_num_pages,
-		'current' => $current,
-		'show_all' => false,
-		'end_size' => $endsize,
-		'mid_size' => $midsize,
-		'type' => $type,
-		'prev_text' => '&lt;&lt;',
-		'next_text' => '&gt;&gt;'
-	);
-
-	if( $wp_rewrite->using_permalinks() )
-		$pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
-
-	if( !empty($wp_query->query_vars['s']) )
-		$pagination['add_args'] = array( 's' => get_query_var( 's' ) );
-
-	return paginate_links( $pagination );
-}
-
 
 /**
  * Mayflower Theme Social Networks
