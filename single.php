356ff8a5273d322bbe68fdc746a3643cd901539c
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-08-17 15:46:50 -0700

    Create Single Page Template and Template Part
    Template at single.php, part at parts/single.php

---
+++ single.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The Single Post Template File
+ *
+ * This displays all single posts
+ *
+ */
+
+get_header(); ?>
+<?php
+/**
+ * Load Variables
+ *
+ */
+global $mayflower_brand;
+$mayflower_options = mayflower_get_options();
+$current_layout = $mayflower_options['default_layout'];
+?>
+
+<div id="content" <?php if ( $mayflower_brand == 'branded' ) {?> class="box-shadow"<?php } ?>>
+
+	<div class="row row-padding">
+
+		<?php if ( has_active_sidebar() ) : ?>
+			<div class="col-md-9 <?php  if ( $current_layout == 'sidebar-content' ) { ?>col-md-push-3<?php } ?>">
+		<?php endif; ?>
+
+				<?php
+				/**
+				 * Get Single Template Part
+				 *
+				 * Check for post type. Look within 'parts/' directory.
+				 */
+				get_template_part( 'parts/single', get_post_type() ); ?>
+
+		<?php if ( has_active_sidebar() ) : ?>
+			</div>
+			<?php get_sidebar();
+		endif; ?>
+	</div>
+
+</div><!-- #content-->
+
+<?php get_footer(); ?>
