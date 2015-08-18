0d5ae07133bd57e005850aed51d27745b544156d
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-08-18 07:55:01 -0700

    Whitespace cleanup for agendas archive and single

--- parts/single-agendas.php
+++ parts/single-agendas.php
@@ -1,37 +1,31 @@
 <?php
+/**
+ * Template for displaying post type for the Trustees Agendas plugin.
+ * https://github.com/BellevueCollege/trustees-agenda
+ */
 
-/*  Template for displaying post type for the Trustees Agendas plugin.
-    https://github.com/BellevueCollege/trustees-agenda
-*/
+if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="content-padding">
 
-if (have_posts()) : while (have_posts()) : the_post();
-?>
-<div class="content-padding">
+		<h1><?php the_title()
 
-    <h1><?php the_title()?>
-    <?php
+			$value = get_post_meta( get_the_ID(), 'meeting_date', true );
 
-    $value = get_post_meta( get_the_ID(), 'meeting_date', true );
+			if( !empty( $value ) ) {
+				$display_date = date('F j, Y', strtotime($value));
 
-    if( !empty( $value ) ) {
-        $display_date = date('F j, Y', strtotime($value));
-
-    ?>
-        <br /><?php echo $display_date; ?></h1>
-    <?php
-    }
-    ?>
-    </h1>
-    <?php
-
-    $content = the_content();
-    if( $content=="" ) :
-    /* Don't display empty the_content or surround divs */
-    else :
-        echo $content;//$content;
-    endif;
-    ?>
-</div>     <!--.content-padding-->
+			?>
+		<br /><?php echo $display_date; ?></h1>
+			<?php } ?>
+		</h1>
+		<?php
+		$content = the_content();
+		if ( $content=="" ) {
+		/* Don't display empty the_content or surround divs */
+		} else {
+			echo $content;
+		} ?>
+	</div> <!--.content-padding-->
 
 <?php
 endwhile;
