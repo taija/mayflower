0423325c0b7d647683cdf129eba9ad0127f23adb
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-09-21 14:36:23 -0700

    Standardize Whitespace in parts/page-nav-page

--- parts/page-nav-page.php
+++ parts/page-nav-page.php
@@ -1,81 +1,63 @@
 <?php while ( have_posts() ) : the_post(); ?>
-
 	<div class="content-padding">
-
 		<h1><?php the_title(); ?></h1>
 
-	<?php if($post->post_content=="") : ?>
-	<!-- Don't display empty the_content or surround divs -->
+		<?php if ( $post-> post_content=="" ) : /* Don't display empty the_content or surround divs */ ?>
 
-	<?php else : ?>
-	<!-- Do stuff when the_content has content -->
+		<?php else : /* Do stuff when the_content has content */ ?>
 			<?php the_content(); ?>
+		<?php endif; ?>
 
-	<?php endif; ?>
-
-
-		<?php
-			endwhile;
-			wp_reset_postdata();
-
-		?>
 	</div><!-- content-padding -->
+<?php endwhile; ?>
 
-	<?php
-		$args = array(
-			'post_type' => 'page',
-			'posts_per_page' => -1,
-			'order' => 'ASC',
-			'orderby' => 'menu_order title',
-			'post_status' => 'publish',
-			'post_parent' => $post->ID
-		);
-		$loop = new WP_Query( $args );
+<?php wp_reset_postdata(); ?>
 
-		//number of columns
-		$columnNum = 3;
-		$count = 0;
-		while ( $loop->have_posts() ) : $loop->the_post();
-			$count++;
-			if ($count == 1) {
-					echo '<div class="row">';
-			}  ?>
+<?php
+$args = array(
+	'post_type'      => 'page',
+	'posts_per_page' => -1,
+	'order'          => 'ASC',
+	'orderby'        => 'menu_order title',
+	'post_status'    => 'publish',
+	'post_parent'    => $post->ID
+);
 
-			<div class="col-md-4 top-spacing15">
-				<div class="content-padding nav-page">
+$loop = new WP_Query( $args );
 
-							<?php
-								if ( has_post_thumbnail() ) { ?>
-									<a class="" href="<?php the_permalink(); ?>">
-										<?php the_post_thumbnail('home-small-ad', array('class' => 'img-responsive')); ?>
-									</a>
+//number of columns
+$columnNum = 3;
+$count = 0;
 
-									<?php
-								}
-								else {}
-							?>
-					<h2>
-						<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-					</h2>
+while ( $loop->have_posts() ) : $loop->the_post();
+	$count++;
+	if ( $count == 1 ) {
+		echo '<div class="row">';
+	} ?>
 
-					<?php
-						the_excerpt();
-						edit_post_link('edit', '<small>', '</small>');
-					?>
-				</div><!-- content-padding .nav-page -->
-			</div><!-- col-md-4 -->  <?php
+	<div class="col-md-4 top-spacing15">
+		<div class="content-padding nav-page">
 
-			if ($count == $columnNum) {
-				echo '</div> <!-- .row -->';
-				$count = 0;
-			}
+			<?php if ( has_post_thumbnail() ) { ?>
+				<a class="" href="<?php the_permalink(); ?>">
+					<?php the_post_thumbnail( 'home-small-ad', array( 'class' => 'img-responsive' ) ); ?>
+				</a>
+			<?php } else {} ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+			<?php
+				the_excerpt();
+				edit_post_link('edit', '<small>', '</small>');
+			?>
+		</div><!-- content-padding .nav-page -->
+	</div><!-- col-md-4 -->
+	<?php if ( $count == $columnNum ) { ?>
+		</div> <!-- .row -->
+		<?php $count = 0;
+	} ?>
 
-		endwhile;
+<?php endwhile; ?>
+<?php if ($count > 0 ) { ?>
+	</div> <!-- .row -->
+<?php } ?>
 
-		if ($count > 0 ) {
-					echo '</div> <!-- .row -->';
-		}
-
-
-		?>
 <?php wp_reset_postdata(); ?>
