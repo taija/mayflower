3ff7f9d52085cf46edc735fe479a0e9df31911f6
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-06 15:07:55 -0800

    Add configurable site scope to search field

--- inc/functions/options-admin.php
+++ inc/functions/options-admin.php
@@ -30,12 +30,37 @@ function mayflower_admin_only_register( $wp_customize ) {
 		'capability' => 'unfiltered_html', //Limit this section to Super-Admin only
 		'sanitize_callback' => 'sanitize_checkbox',
 	) );
+	$wp_customize->add_setting( 'limit_searchform_scope' , array(
+		'default'     => false,
+		'transport'   => 'refresh',
+		'capability' => 'unfiltered_html', //Limit this section to Super-Admin only
+		'sanitize_callback' => 'sanitize_checkbox',
+	) );
+	$wp_customize->add_setting( 'custom_searchform_scope' , array(
+		'transport'   => 'refresh',
+		'capability' => 'unfiltered_html', //Limit this section to Super-Admin only
+		'sanitize_callback' => 'sanitize_text_field',
+	) );
 	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'hide_searchform', array(
 		'label'        => __( 'Hide Search Form', 'hide_searchform' ),
 		'description'  => __( 'Hide Search in Mayflower Lite' ),
 		'section'      => 'mayflower_admin_options',
 		'settings'     => 'hide_searchform',
 		'type'         => 'checkbox',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'limit_searchform_scope', array(
+		'label'        => __( 'Limit Search Form Scope', 'limit_searchform_scope' ),
+		'description'  => __( 'Search within the subsite instead of within the master BC scope' ),
+		'section'      => 'mayflower_admin_options',
+		'settings'     => 'limit_searchform_scope',
+		'type'         => 'checkbox',
+	) ) );
+	$wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'custom_searchform_scope', array(
+		'label'        => __( 'Custom Search Form Scope', 'custom_searchform_scope' ),
+		'description'  => __( 'Allows definition of custom scope. If none is defined, local site URL will be used. ' ),
+		'section'      => 'mayflower_admin_options',
+		'settings'     => 'custom_searchform_scope',
+		'type'         => 'text',
 	) ) );
 }
 add_action( 'customize_register', 'mayflower_admin_only_register' );
--- searchform.php
+++ searchform.php
@@ -1,9 +1,41 @@
-<form action="//www.bellevuecollege.edu/search/" method="get" class="form-search">
+<?php
+	$search_url              = '//www.bellevuecollege.edu/search/';
+	$site_url                = get_site_url();
+	$limit_searchform_scope  = get_theme_mod( 'limit_searchform_scope' );
+	$custom_searchform_scope = get_theme_mod( 'custom_searchform_scope' );
+	
+	/* Set Scope to Site URL if not otherwise defined */
+	if ( empty( $custom_searchform_scope ) ) {
+		$custom_searchform_scope = "[site:$site_url]";
+	}
+?>
+<form action="<?php echo $search_url; ?>" method="get" class="form-search" id="bc-search">
 	<div class="input-group pull-right" role="search">
-	  <input type="text" value="" name="txtQuery" class="form-control" id="college-search-field">
-	  <label class="sr-only" for="college-search-field">Search</label>
-	  <span class="input-group-btn">
-			<button type="submit" class="btn btn-default" name="search" id="college-search-submit">Search</button>
-	  </span>
-	</div><!-- input-group -->
+		<input type="text" value="" name="txtQuery" class="form-control" id="college-search-field">
+		<div class="input-group-btn">
+			<button type="submit" class="btn btn-default" id="college-search-submit">Search</button>
+			<?php if ( $limit_searchform_scope ) : ?>
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<span class="caret"></span>
+					<span class="sr-only">More Search Options</span>
+				</button>
+				<div class="dropdown-menu dropdown-menu-right">
+					<a href="<?php echo $search_url; ?>" class="btn btn-link" id="college-search-submit-all">Search all of Bellevue College</a>
+				</div>
+				<script type="text/javascript">
+					/* Insert limiting search terms on submit */
+					jQuery( "#bc-search" ).submit(function( event ) {
+						jQuery('#college-search-field').val('<?php echo $custom_searchform_scope; ?> ' + jQuery('#college-search-field').val());
+					});
+					/* Generate search URL in dropdown */
+					jQuery('#college-search-submit-all').click( function( event ) {
+						/* Default action is to simply go to search page, if no JS */
+						event.preventDefault();
+						var url = "<?php echo $search_url; ?>?txtQuery=" + jQuery( '#college-search-field' ).val(); 
+						jQuery(location).attr('href', url);
+					});
+				</script>
+			<?php endif; ?>
+		</div>
+	</div>
 </form>
