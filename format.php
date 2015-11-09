036e8a58fdaa2223b0d174ef55e86c0acbb6947a
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-09 14:24:24 -0800

    Revise format.php to standardize image alignment
    Remove Media Object markup, and restyle captions and alignment types

--- format.php
+++ format.php
@@ -1,29 +1,24 @@
-<h2 <?php post_class() ?>>
-	<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-</h2>
-<small><?php _e('Date posted:', 'mayflower'); ?> <?php echo get_the_date(); ?></small>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<h2 <?php post_class() ?>>
+		<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+	</h2>
+	<p><small><?php _e('Date posted:', 'mayflower'); ?> <?php echo get_the_date(); ?></small></p>
+	<?php if ( has_post_thumbnail() ) :
 
-<div class="media">
-	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="pull-left wp-caption">
-			<?php the_post_thumbnail( 'medium', array( 'class' => 'media-object' ) );
-			if ( get_post( get_post_thumbnail_id() )->post_excerpt ) :
-				$tn_id = get_post_thumbnail_id( $post->ID );
-				$img = wp_get_attachment_image_src( $tn_id, 'medium' );
-				$width = $img[1]; ?>
+		if ( get_post( get_post_thumbnail_id() )->post_excerpt ) :
 
-					<p class="featured-caption media-object wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
+			$tn_id = get_post_thumbnail_id( $post->ID );
+			$img = wp_get_attachment_image_src( $tn_id, 'medium' );
+			$width = $img[1]; ?>
 
-			<?php endif; ?>
-		</div><!-- wp-caption -->
-	<?php endif; ?>
-	<div class="media-body">
-		<div class="media-content">
-			<?php the_excerpt(); ?>
-		</div><!-- media-content -->
-		<?php if ( is_single( $post ) ) : ?>
+			<figure class="img-thumbnail alignleft wp-caption">
+				<?php the_post_thumbnail( 'medium' ); ?>
+				<figcaption class="featured-caption wp-caption-text"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
+			</figure><!-- wp-caption -->
 		<?php else : ?>
+			<?php the_post_thumbnail( 'medium', array( 'class' => 'img-thumbnail alignleft' ) ); ?>
 		<?php endif; ?>
-	</div><!-- media-body -->
-</div><!-- media -->
-<hr />
+	<?php endif; ?>
+	<?php the_excerpt(); ?>
+	<hr>
+</article>
