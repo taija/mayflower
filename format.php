cee99e485eb4c58071918d123aad036faa46922f
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-09 10:23:55 -0800

    Fix whitespace in fomat.php

--- format.php
+++ format.php
@@ -1,48 +1,29 @@
-	<h2 <?php post_class() ?>>
-		<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-	</h2>
+<h2<?php post_class() ?>>
+	<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
+</h2>
+<small>Date posted: <?php echo get_the_date(); ?></small>
 
-	<small>Date posted: <?php echo get_the_date(); ?></small>
+<div class="media">
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="pull-left wp-caption">
+			<?php the_post_thumbnail( 'medium', array( 'class' => 'media-object' ) );
+			if ( get_post( get_post_thumbnail_id() )->post_excerpt ) :
+				$tn_id = get_post_thumbnail_id( $post->ID );
+				$img = wp_get_attachment_image_src( $tn_id, 'medium' );
+				$width = $img[1]; ?>
 
+					<p class="featured-caption media-object wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
 
-		<div class="media">
-<?php
-if ( has_post_thumbnail() ) {
-	?>
-
-	 <div class="pull-left wp-caption">
-		<?php
-			the_post_thumbnail('medium', array('class' => 'media-object'));
-				if(get_post(get_post_thumbnail_id())->post_excerpt) {
-                    $tn_id = get_post_thumbnail_id( $post->ID );
-
-                    $img = wp_get_attachment_image_src( $tn_id, 'medium' );
-                    $width = $img[1];
-                    ?>
-				<p class="featured-caption media-object wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
-				<?php } ?>
-	 </div><!-- wp-caption -->
-<?php
-	}
-	else {	}
-?>
-
-			<div class="media-body">
-
-				<div class="media-content">
-					<?php the_excerpt(); ?>
-				</div><!-- media-content -->
-			    <?php
-				if (is_single($post)){
-				?>
-
-		        <?php
-				} else {
-				?>
-		        <?php
-
-				}
-				?>
-		    </div><!-- media-body -->
-		</div><!-- media -->
-	<hr />
+			<?php endif; ?>
+		</div><!-- wp-caption -->
+	<?php endif; ?>
+	<div class="media-body">
+		<div class="media-content">
+			<?php the_excerpt(); ?>
+		</div><!-- media-content -->
+		<?php if ( is_single( $post ) ) : ?>
+		<?php else : ?>
+		<?php endif; ?>
+	</div><!-- media-body -->
+</div><!-- media -->
+<hr />
