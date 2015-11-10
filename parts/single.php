0ba03ce259b8e388b48de4335560b31da68db9ce
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-10 07:47:43 -0800

    Change image formatting in parts/single

--- parts/single.php
+++ parts/single.php
@@ -1,25 +1,25 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="content-padding">
 		<h1><?php the_title();?></h1>
-		<div>
-			<small>Date posted: <?php the_date(); ?></small>
-		</div>
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="pull-left wp-caption single-featured-thumb">
-				<?php the_post_thumbnail( 'medium', array( 'class' => 'media-object' ) );
-				if ( get_post( get_post_thumbnail_id() )->post_excerpt ) {
+		<p class="entry-date">Date posted: <?php the_date(); ?></p>
 
-					$tn_id = get_post_thumbnail_id( $post->ID );
-					$img = wp_get_attachment_image_src( $tn_id, 'medium' );
-					$width = $img[1]; ?>
+		<?php if ( has_post_thumbnail() && get_post_format() != 'video' ) :
 
-					<p class="featured-caption media-object wp-caption-text" style="width:<?php echo $width.'px';?>">
-						<?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?>
-					</p>
-				<?php } ?>
-			</div>
-		<?php else :
-		endif; ?>
+			if ( get_post( get_post_thumbnail_id() )->post_excerpt ) :
+
+				$tn_id = get_post_thumbnail_id( $post->ID );
+				$img = wp_get_attachment_image_src( $tn_id, 'medium' );
+				$width = $img[1]; ?>
+
+				<figure class="img-thumbnail alignleft wp-caption">
+					<?php the_post_thumbnail( 'medium' ); ?>
+					<figcaption class="featured-caption wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
+				</figure><!-- wp-caption -->
+			<?php else : ?>
+				<?php the_post_thumbnail( 'medium', array( 'class' => 'img-thumbnail alignleft' ) ); ?>
+			<?php endif; ?>
+		<?php endif; ?>
+
 		<?php the_content(); ?>
 	</div><!-- content-padding -->
 
