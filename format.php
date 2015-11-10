364fbb657ab1bf51b08d33bc6ddeb31db3fc90bc
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-10 07:46:58 -0800

    Add width and clearfix to formats

--- format.php
+++ format.php
@@ -1,8 +1,8 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<h2 <?php post_class() ?>>
 		<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
 	</h2>
-	<p><small><?php _e('Date posted:', 'mayflower'); ?> <?php echo get_the_date(); ?></small></p>
+	<p class="entry-date"><?php _e('Date posted:', 'mayflower'); ?> <?php echo get_the_date(); ?></p>
 	<?php if ( has_post_thumbnail() ) :
 
 		if ( get_post( get_post_thumbnail_id() )->post_excerpt ) :
@@ -13,12 +13,12 @@
 
 			<figure class="img-thumbnail alignleft wp-caption">
 				<?php the_post_thumbnail( 'medium' ); ?>
-				<figcaption class="featured-caption wp-caption-text"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
+				<figcaption class="featured-caption wp-caption-text" style="width:<?php echo $width.'px';?>"><?php echo get_post( get_post_thumbnail_id() )->post_excerpt ?></p>
 			</figure><!-- wp-caption -->
 		<?php else : ?>
 			<?php the_post_thumbnail( 'medium', array( 'class' => 'img-thumbnail alignleft' ) ); ?>
 		<?php endif; ?>
 	<?php endif; ?>
 	<?php the_excerpt(); ?>
-	<hr>
 </article>
+<hr>
