4e4fdd9f9be6bf82376eac1f7dffc13b063e7462
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-23 09:58:16 -0800

    Add Post Data Display Configuration
    * Allow admins to choose to display post date and post author on posts.
      Post title is enabled by default, post author disabled.
    * Fix bug where post author did not display on author archive page

--- format-video.php
+++ format-video.php
@@ -1,8 +1,25 @@
+<?php
+// Load Mayflower options into array
+$mayflower_options = mayflower_get_options();
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<h2 <?php post_class() ?>>
 		<a href="<?php the_permalink(); ?>"><?php the_title();?></a>
 	</h2>
-	<p class="entry-date"><?php _e('Date posted:', 'mayflower'); ?> <?php echo get_the_date(); ?></p>
+	<?php // Check if post date or author should be displayed
+	if ( $mayflower_options['display_post_author'] || $mayflower_options['display_post_date'] ) : ?>
+		<p class="entry-date">
+			<?php //Check if post date should be displayed
+			if ( $mayflower_options['display_post_date'] ) : ?>
+				<?php _e( 'Date posted: ', 'mayflower');
+				the_date(); ?>
+			<?php endif;
+			// Check if post author should be displayed
+			if ( $mayflower_options['display_post_author'] ) : ?>
+				&nbsp;<span class="pull-right"><?php _e( 'Author: ', 'mayflower' ) ?><?php the_author(); ?></span>
+			<?php endif; ?>
+		</p>
+	<?php endif; ?>
 
 	<div class="video-container">
 		<?php the_content(); ?>
