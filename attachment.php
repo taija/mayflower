e27a77d03bf98b00a3369d8ca6ffd481072e0754
Taija Tevia-Clark <taija.tevia-clark@bellevuecollege.edu>
2015-11-24 15:37:01 -0800

    Whitespace cleanup in attachment.php

--- attachment.php
+++ attachment.php
@@ -29,28 +29,36 @@ $caption           = $post->post_excerpt;
 
 				<?php if (have_posts()) : while (have_posts()) : the_post(); /* Template format for images */ ?>
 					<div class="content-padding">
-						<?php if ( wp_attachment_is_image( $post->id ) ) : ?>
-							<h2><?php the_title();?></h2>
+						<?php
+						/*
+						 * Check if attachment is an image
+						 */
+						if ( wp_attachment_is_image( $post->id ) ) : ?>
+							<h1><?php the_title();?></h1>
 							&nbsp;
 							<div class="wp-caption">
-								<a href="<?php echo wp_get_attachment_url($post->id); ?>" title="<?php the_title(); ?>" rel="attachment">
-									<?php echo wp_get_attachment_image($post->id, 'large', false, array('class' => 'attachment-large img-responsive aligncenter')); ?>
+								<a href="<?php echo wp_get_attachment_url( $post->id ); ?>" title="<?php the_title(); ?>" rel="attachment">
+									<?php echo wp_get_attachment_image( $post->id, 'large', false, array( 'class' => 'attachment-large img-responsive aligncenter' ) ); ?>
 								</a>
-								<?php if ( !empty($caption) ) { ?>
+								<?php if ( !empty( $caption ) ) { ?>
 										<p class="wp-caption-text"><?php echo $caption; ?></p>
 								<?php } ?>
 							</div>
-							<?php if ( !empty($description) ) { ?>
+							<?php if ( !empty( $description ) ) { ?>
 								<p><?php echo $description; ?></p>
 							<?php } ?>
-						<?php else : /* Display format for non-image files */?>
-							<h2>Download File</h2>
+						<?php
+						/*
+						 * If attachment is non-image
+						 */
+						else : ?>
+							<h1>Download File</h1>
 							<div class="media">
 								<div class="media-left">
-									<?php echo wp_get_attachment_image($post->id, 'thumbnail', true, array('class' => 'media-object')); ?>
+									<?php echo wp_get_attachment_image( $post->id, 'thumbnail', true, array( 'class' => 'media-object' ) ); ?>
 								</div>
 								<div class="media-body">
-									<h4 class="media-heading"><a href="<?php echo wp_get_attachment_url($post->ID) ?>"><?php the_title();?></a></h4>
+									<h4 class="media-heading"><a href="<?php echo wp_get_attachment_url( $post->ID ) ?>"><?php the_title();?></a></h4>
 									<?php if ( !empty($description) ) { ?>
 										<p><?php echo $description; ?></p>
 									<?php } ?>
